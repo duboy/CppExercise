a461fb50c193de40eecda55f0058b0cd45d65298
duboy <futuredby@gmail.com>
2017-12-04 17:33:05 +0800

    3ms 512k

--- 剑指Offer_编程题/NumberOf1.cpp
+++ 剑指Offer_编程题/NumberOf1.cpp
@@ -4,19 +4,9 @@ using namespace std;
 
 class Solution {
 public:
-     int  NumberOf1(int n) {
-         if (n > 0)
-         {
-             bitset<32> b(n);
-             return b.count();
-         }
-         else
-         {
-             bitset<32> b(-n);
-             int temp = (b.flip()).to_ulong() + 1;
-             bitset<32> b2(temp);
-             return b2.count();
-         }             
+     int  NumberOf1(int n) {    
+         bitset<32> b(n);
+         return b.count();
      }
 };
 
