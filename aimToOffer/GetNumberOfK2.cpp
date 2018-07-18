0b7fd7a339f5163c464a0004e3e2016b55ff077a
duboy <futuredby@gmail.com>
2018-07-18 21:23:02 +0800

    7-18 night

--- aimToOffer/GetNumberOfK2.cpp
+++ aimToOffer/GetNumberOfK2.cpp
@@ -17,8 +17,8 @@ public:
 		
 		firstK = getFirstK(data, start, end, k);
 		
-		int numOfK = 0;
-		for (; firstK < data.size(); ++firstK)
+		int numOfK = -1;
+		for (numOfK = 0; firstK < data.size(); ++firstK)
 		{
 			if (data[firstK] == k)
 				++numOfK;
@@ -26,23 +26,27 @@ public:
 				return numOfK;
 		}
 		
-		return -1;
+		return numOfK;
     }
 	
 	int getFirstK(vector<int> &data, int start, int end, int k)
 	{
 		int mid = (end + start) / 2;
 		
-		if (start == end)
-			return start;
+		if (start > end)
+			return -1;
 		
-		if (end - start == 1)
-			return data[start] == k ? start : end;
-		
-		if (data[mid] >= k)
-			end = mid;
+		if (data[mid] == k)
+		{
+			if ( mid == 0 || (mid > 0 && data[mid - 1] != k))
+				return mid;
+			else 
+				end = mid - 1;
+		}
+		else if (data[mid] > k)
+			end = mid - 1;
 		else if (data[mid] < k)
-			start = mid;
+			start = mid + 1;
 		
 		return getFirstK(data, start, end, k);
 	}
