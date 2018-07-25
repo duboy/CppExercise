f6aeced9d993207d61260cab2b9621f17c621c06
duboy <futuredby@gmail.com>
2018-07-25 17:22:20 +0800

    18-7-25

--- aimToOffer/hasPath.cpp
+++ aimToOffer/hasPath.cpp
@@ -11,14 +11,24 @@ class Solution {
 public:
     bool hasPath(char* matrix, int rows, int cols, char* str)
     {
-		
+		if (matrix == nullptr || rows < 1 || cols < 1 || str == nullptr)
+			return false;
+
+		bool *visited = new bool[rows * cols];
+		memset(visited, 0, rows * cols);
+
+		int pathLength = 0;
+		for (int row = 0; row < rows; ++row)
+		{
+			for ()
+		}
     }
 };
 
 int main()
 {
 	char matrix[] = {'a', 'b', 'c', 'e', 's', 'f', 'c', 's', 'a', 'd', 'e', 'e'};
-
+	char str[] = "bcced";
 	Solution sol;
-	cout << sol.hasPath(matrix, 3, 4);
+	cout << sol.hasPath(matrix, 3, 4, str);
 }
