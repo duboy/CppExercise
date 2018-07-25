acaacf28bc7e707a8ae59670d574199807277b63
duboy <futuredby@gmail.com>
2018-07-25 21:11:32 +0800

    rm cached

--- aimToOffer/hasPath.cpp
+++ aimToOffer/hasPath.cpp
@@ -20,9 +20,51 @@ public:
 		int pathLength = 0;
 		for (int row = 0; row < rows; ++row)
 		{
-			for ()
+			for (int col = 0; col < cols; ++col)
+			{
+				if (hasPathCore(matrix, rows, cols, row, col, str, pathLength, visited))
+				{
+					return true;
+				}
+			}
 		}
+
+		delete[] visited;
+		return false;
+
     }
+
+	bool hasPathCore(const char *matrix, int rows, int cols, int row,
+		int col, const char *str, int &pathLength, bool *visited)
+	{
+		if (str[pathLength] == '\0')
+			return true;
+
+		bool hasPath = false;
+
+		if (row >= 0 && row < rows && col >= 0 && col < cols
+			&& matrix[row * cols + col] == str[pathLength] && !visited[row * cols + col])
+		{
+			++pathLength;
+			visited[row * cols + col] = true;
+
+			hasPath = hasPathCore(matrix, rows, cols, row, col - 1,
+				str, pathLength, visited)
+				|| hasPathCore(matrix, rows, cols, row - 1, col,
+					str, pathLength, visited)
+				|| hasPathCore(matrix, rows, cols, row, col + 1,
+					str, pathLength, visited)
+				|| hasPathCore(matrix, rows, cols, row + 1, col,
+					str, pathLength, visited);
+
+			if (!hasPath)
+			{
+				--pathLength;
+				visited[row * cols + col] = false;
+			}					
+		}
+		return hasPath;
+	}
 };
 
 int main()
