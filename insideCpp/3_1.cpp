83993ce57078b1acb8b52e944a9cd16e36aa3c91
duboy <futuredby@gmail.com>
2018-07-28 22:06:32 +0800

    7-28 night

--- insideCpp/3_1.cpp
+++ insideCpp/3_1.cpp
@@ -9,14 +9,53 @@ using namespace std;
 class X {};
 class Y : public virtual X {};
 class Z : public virtual X {};
-class A : public Y, public Z {};
+class A : public Y, public Z 
+{
+private:
+	char ch; 
+	int b;
+	
+protected:
+	int c;
+public:
+	int a;
+private:
+	int a1;
+
+};
+
+typedef int length;
+
+class Point3d
+{
+public:
+	Point3d();
+	~Point3d();
+	void numble(length val) { _val = val; }
+	length numble() { return _val; }
+
+private:
+	typedef float length;
+	length _val;
+};
+
+Point3d::Point3d()
+{
+}
+
+Point3d::~Point3d()
+{
+}
 
 int main()
 {
 	X x;
 	Y y;
-	Z z;
 	A a;
+	Z z;
+
+
+	
 	cout << sizeof(z);
 	return 0;
 }
